fe25a7ac54be07f012cb2aa409da6eb6e95eea8f
RyanSitorus <Andriansitorus1@gmail.com>
2024-09-21 19:43:27 +0700

    initial commit

---
+++ src/main/java/com/srss_backend/SrssBackendApplication.java
@@ -0,0 +1,13 @@
+package com.srss_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrssBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrssBackendApplication.class, args);
+	}
+
+}
